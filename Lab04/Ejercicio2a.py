4b80dbc64ca3db9373a88f19d088aa1d91df1120
klismannSis <kchancuana@unsa.edu.pe>
2023-06-04 23:32:19 -0500

    /Iplementando Codigo.

--- Lab04/Ejercicio2a.py
+++ Lab04/Ejercicio2a.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+knightP = knight.negative()
+line1 = knight.join(knightP)
+
+line2 = knightP.join(knight)
+final = line1.up(line2)
+draw(final)
