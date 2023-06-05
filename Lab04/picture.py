b639dd53f6e57d823daa35c5cbd850c37d797364
klismannSis <kchancuana@unsa.edu.pe>
2023-06-04 23:01:42 -0500

    /Codigo Completado.

--- Lab04/picture.py
+++ Lab04/picture.py
@@ -1,4 +1,4 @@
-from colors import *.
+from colors import *
 class Picture:
   def __init__(self, img):
     self.img = img;
@@ -16,46 +16,47 @@ class Picture:
     vertical = []
     for value in self.img:
     	vertical.append(value[::-1])
-    return Picture(vertical) """Se agrego Picture..."""
-"""__________________________________________________________"""
+    return Picture(vertical) 
+
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
     horizontal = []
-    for row in self.img: """Se implemento for...."""
-      horizontal.insert(0, row)
+    for row in self.img: """Permite obtener una versión reflejada horizontalmente de una imagen."""
+    horizontal.insert(0, row)
     return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
     negative = [
       ''.join(self._invColor(char) for char in value)
-      for value self.img
+    for value in self.img
     ]
     return Picture(negative)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    join = []
+    join = []  
+    """Almacenará las filas combinadas de las dos imágenes."""
     for i in range(len(self.img)):
-      join.append(self.img[i] + "" + p.img[i])  """Completando  el codigo..."""
+      join.append(self.img[i] + "" + p.img[i])  
     return Picture(join)
-
-  def up(self, p):             """Completo..."""
-    image = self.img + p.img 
-    return Picture(image)
+"""Permite combinar verticalmente dos imágenes, apilando una encima de la otra."""
+def up(self, p):
+  image = self.img + p.img
+  return Picture(image)
 
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
         figura actual """
     image = []
     for i in range(len(self.img)):
-      line = ""
+      line = ""   """almacenará la fila combinada."""
       for j in range(self.img[i]):
-        if p.img[i][j] != " ":  """Completando..."""
-          line + = p,img[i][j]
+        if p.img[i][j] != " ":  
+          line += p.img[i][j]
         else:
-          line + = self.img[i][j]
+          line += self.img[i][j]
       image.append(line)
     return Picture(image)
   
@@ -68,7 +69,8 @@ class Picture:
     return Picture(image)
 
   def verticalRepeat(self, n):
-    image = self.img * n
+    image = self.img * n       
+    """permite repetir verticalmente la imagen actual"""
     return Picture(image)
 
   #Extra: Sólo para realmente viciosos 
