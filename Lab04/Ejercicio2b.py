80eaad4fc86363abc23f64989363f27e742949fa
klismannSis <kchancuana@unsa.edu.pe>
2023-06-06 01:42:55 -0500

    C:/Program Files/Git/Comletando...

--- Lab04/Ejercicio2b.py
+++ Lab04/Ejercicio2b.py
@@ -2,10 +2,10 @@ from interpreter import draw
 from chessPictures import *
 
 # Obtener la representación negativa del knight
-knightN = knight.negative()
-line1 = knight.join(knightN)
-knightN2 = knightN.verticalMirror() # Obtener la versión espejada verticalmente de knightN
-line2 = knight.verticalMirror().join(knightN2)
+knightA = knight.negative()
+line1 = knight.join(knightA)
+knightA2 = knightA.verticalMirror() # Obtener la versión espejada verticalmente de knightN
+line2 = knight.verticalMirror().join(knightA2)
 final = line1.up(line2)
 
 draw(final)
