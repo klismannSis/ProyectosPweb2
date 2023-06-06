497e39fee4782204c34a4a912684e9ef659340ef
klismannSis <kchancuana@unsa.edu.pe>
2023-06-05 19:57:28 -0500

    /Corrigiendo errores.

--- Lab04/Ejercicio2a.py
+++ Lab04/Ejercicio2a.py
@@ -1,9 +1,9 @@
 from interpreter import draw
 from chessPictures import *
 
-knightP = knight.negative()
-line1 = knight.join(knightP)
+caballo1 = knight.negative()
+line1 = knight.join(caballo1)
 
-line2 = knightP.join(knight)
+line2 = caballo1.join(knight)
 final = line1.up(line2)
 draw(final)
--- Lab04/Ejercicio2d.py
+++ Lab04/Ejercicio2d.py
@@ -1,2 +1,4 @@
 from interpreter import draw
 from chessPictures import *
+
+draw(square.join(square.negative()).horizontalRepeat(4))
