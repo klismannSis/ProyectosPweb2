7f5a799de28b47d45389acf7527bb8a7fb967ddf
klismannSis <kchancuana@unsa.edu.pe>
2023-06-05 21:00:30 -0500

    .........

--- Lab04/Ejercicio2e.py
+++ Lab04/Ejercicio2e.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+# Obtener la representación negativa del cuadrado
+square_negative = square.negative()
+concatenated = square_negative.join(square)
+repeated = concatenated.horizontalRepeat(4)  # Repetir horizontalmente el patrón cuatro veces
+
+draw(repeated)
